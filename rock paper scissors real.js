1b2075a4faa5f757509882a0d4038cfa6ce4c9c8
Antboi123 <sunithadevnarian@gmail.com>
2024-12-07 11:03:08 +0200

    Add files via upload

---
+++ rock paper scissors real.js
@@ -0,0 +1,24 @@
+function playGame(playerChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    let result = '';
+
+    if (playerChoice === computerChoice) {
+        result = "It's a draw!";
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        result = "You win!";
+    } else {
+        result = "You lose!";
+    }
+
+    document.getElementById('result-text').textContent = `You chose ${playerChoice}. Computer chose ${computerChoice}. ${result}`;
+}
+
+
+
+
